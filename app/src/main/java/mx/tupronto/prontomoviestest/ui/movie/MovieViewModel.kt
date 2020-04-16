8a92c16f60fe894ec8f80c0016ddbfad1cfed819
Irving Lara <irving@gaiadesign.com.mx>
2020-04-16 13:34:35 -0500

    create bottom navigation whit favorite & movie - view model

---
+++ app/src/main/java/mx/tupronto/prontomoviestest/ui/movie/MovieViewModel.kt
@@ -0,0 +1,13 @@
+package mx.tupronto.prontomoviestest.ui.movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MovieViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is movie Fragment"
+    }
+    val text: LiveData<String> = _text
+}
